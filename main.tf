0b8664ff84eb7038344eca60bcf8a1a091b11893
Athina Thomas <anonymousartistvvs@protonmail.com>
2023-09-21 18:23:37 +0000

    #13 Added the S3 provider to the main.tf file

--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
@@ -12,8 +17,15 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 32
   special          = false
+  lower            = true
+  upper            = false
+}
+
+# Creating a S3 bucket and getting the output name and placing it as the value.
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
