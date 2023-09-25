dc7c8929ca2a975867a25b27ea48144011c68c33
Athina Thomas <anonymousartistvvs@protonmail.com>
2023-09-25 17:11:49 +0000

    #25 organized the root structure module.

---
+++ providers.tf
@@ -0,0 +1,38 @@
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ArtistUniverse"#
+
+  #  workspaces {
+  #    name = "terra-house"
+  #  }
+  #}
+
+ # cloud {
+ #   organization = "ArtistUniverse"
+#
+ #   workspaces {
+ #     name = "terra-house"
+ #   }
+ # }
+  
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+
+     aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
+  # Configuration options
+}
