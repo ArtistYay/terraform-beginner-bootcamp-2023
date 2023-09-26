21918ebdae6e22bc883ef3a563b0247b79100327
Artist <anonymousartistvvs@protonmail.com>
2023-09-26 14:45:29 -0400

    #27 comment out random block and imported bucket. (#28)

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-resource "random_string" "bucket_name" {
-  length           = 32
-  special          = false
-  lower            = true
-  upper            = false
-}
+#resource "random_string" "bucket_name" {
+#  length           = 32
+#  special          = false
+#  lower            = true
+#  upper            = false
+#}
 
-# Creating a S3 bucket and getting the output name and placing it as the value.
-resource "aws_s3_bucket" "example" {
-  bucket = random_string.bucket_name.result
+# Creating a S3 bucket and getting the name from a variable.
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
 
   tags = {
     UserUuid = var.user_uuid
--- providers.tf
+++ providers.tf
@@ -16,23 +16,23 @@ terraform {
  #   }
  # }
   
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-
-     aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
+  #required_providers {
+  #  random = {
+  #    source = "hashicorp/random"
+  #    version = "3.5.1"
+  #  }
+#
+  #   aws = {
+  #    source = "hashicorp/aws"
+  #    version = "5.17.0"
+  #  }
+  #}
 }
 
 provider "aws" {
   # Configuration options
 }
 
-provider "random" {
-  # Configuration options
-}
+#provider "random" {
+#  # Configuration options
+#}
