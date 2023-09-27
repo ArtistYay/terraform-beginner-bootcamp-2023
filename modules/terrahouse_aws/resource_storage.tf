ec6a1ea506f59fa1f774a3039ec3231838cca613
Athina Thomas <anonymousartistvvs@protonmail.com>
2023-09-27 23:06:00 +0000

    #33 created CDN and serve S3 bucket from CDN

---
+++ modules/terrahouse_aws/resource_storage.tf
@@ -0,0 +1,58 @@
+# Creating an S3 bucket and getting the name from a variable.
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "index.html"
+  source = var.index_html_filepath # "${path.root}/public/index.html"
+  content_type = "text/html"
+  etag = filemd5(var.index_html_filepath)
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = var.error_html_filepath 
+  content_type = "text/html"
+  etag = filemd5(var.error_html_filepath)
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  policy = jsonencode({
+    "Version" = "2012-10-17",
+    "Statement" = {
+      "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
+      "Effect" = "Allow",
+      "Principal" = {
+        "Service" = "cloudfront.amazonaws.com"
+      },
+      "Action" = "s3:GetObject",
+      "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+      "Condition" = {
+      "StringEquals" = {
+          "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+          #"AWS:SourceArn": data.aws_caller_identity.current.arn
+        }
+      }
+    }
+  })
+}
