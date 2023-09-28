f53dd4c0c6bdccb1c795de7d572d467cfe564d91
Athina Thomas <anonymousartistvvs@protonmail.com>
2023-09-28 19:44:38 +0000

    #35 gave the index.html a content version.

--- modules/terrahouse_aws/resource_storage.tf
+++ modules/terrahouse_aws/resource_storage.tf
@@ -25,6 +25,10 @@ resource "aws_s3_object" "index_html" {
   source = var.index_html_filepath # "${path.root}/public/index.html"
   content_type = "text/html"
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 
 resource "aws_s3_object" "error_html" {
@@ -33,6 +37,9 @@ resource "aws_s3_object" "error_html" {
   source = var.error_html_filepath 
   content_type = "text/html"
   etag = filemd5(var.error_html_filepath)
+  lifecycle {
+    ignore_changes = [etag]
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -55,4 +62,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
