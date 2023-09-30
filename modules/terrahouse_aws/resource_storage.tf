47bd123c6205338938298d1e2608c5e51aa56493
Athina Thomas <anonymousartistvvs@protonmail.com>
2023-09-30 17:33:34 +0000

    new message

--- modules/terrahouse_aws/resource_storage.tf
+++ modules/terrahouse_aws/resource_storage.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "website_bucket" {
 
   tags = {
     UserUuid = var.user_uuid
-    Hello = "world"
+    Hello = "mars"
   }
 }
 
@@ -23,17 +23,17 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
-  source = var.index_html_filepath # "${path.root}/public/index.html"
+  source = "${path.root}${var.index_html_filepath}" # "${path.root}/public/index.html"
   content_type = "text/html"
-  etag = filemd5(var.index_html_filepath)
+  etag = filemd5("${path.root}${var.index_html_filepath}")
 }
 
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
-  source = var.error_html_filepath 
+  source = "${path.root}${var.error_html_filepath}"
   content_type = "text/html"
-  etag = filemd5(var.error_html_filepath)
+  etag = filemd5("${path.root}${var.error_html_filepath}")
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
