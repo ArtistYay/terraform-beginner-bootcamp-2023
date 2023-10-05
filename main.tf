1d6667023b892fb4bfc787b4175a13140bc430c2
Athina Thomas <anonymousartistvvs@protonmail.com>
2023-10-05 23:11:47 +0000

    N/A

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ terraform {
 #  # Configuration options
 }
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -46,3 +46,14 @@ provider "terratowns" {
 #  content_version = var.content_version
 #  assets_path = var.assets_path
 #}
+
+resource "terratowns_home" "home" {
+  name = "Destiny 2"
+  description = <<DESCRIPTION
+Destiny 2
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
